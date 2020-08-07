be5d76c158964264ad8b10d0d669d5729ee113ee
cortana <mf2u19@soton.ac.uk>
2020-08-07 17:43:28 +0300

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,43 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning: "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors."
+  },
+  {
+    id: 5,
+    emoji: "➰",
+    name: "Curly Loop",
+    meaning: "The “curly loop“ originates from Japan. Looks like an astrological symbol and is also used as a secret code. Shhh, do not tell anyone!"
+  },
+  {
+    id: 6,
+    emoji: "💎",
+    name: "Gem Stone",
+    meaning: "Diamonds are a girl's best friends. You are important to me and as precious as a diamond. Diamonds are for eternity. Symbol of luxury and wealth."
+  }
+];
+
+export default emojipedia;
